75e36658c157de8ee0802163ad851769a7388597
gionnimontana <gionnimontana@gmail.com>
2026-01-06 00:14:50 +0100

    Update initial year net worth and year goal target in portfolio calculations

--- src/api/index.js
+++ src/api/index.js
@@ -5,21 +5,21 @@ const getAssetsSchema = async () => {
             ['Equity', 'LU1829221024', 1520, 'Nasdaq ETF'],
             ['Equity', 'IE00B4L5Y983', 1029, 'All World ETF'],
             ['Equity', 'LU1900068914', 197, 'MSCI China ETF'],
-            ['Equity', 'GB00BJYDH287', 60, 'Physical Bitcoin'],
-            ['Equity', 'DE000A2T0VU5', 18, 'Physical Gold'],
-            ['Crypto', 'BTC', 0.69268, 'Bitcoin'],
+            ['Equity', 'IE000JJPY166', 350, 'ETF monetario'],
+            ['Equity', 'GB00BJYDH287', 277, 'Physical Bitcoin'],
+            ['Crypto', 'BTC', 0.68268, 'Bitcoin'],
             ['Crypto', 'ETH', 13.39, 'Ethereum'],
             ['Crypto', 'USDT', 462, 'USDT'],
             ['Liquidity', 'conto arancio', 50764, 'ING'],
             ['Liquidity', 'bbva', 891, 'Bbva'],
-            ['Liquidity', 'fineco', 8665, 'Fineco'],
+            ['Liquidity', 'fineco', 2742, 'Fineco'],
             ['Liquidity', 'revolut', 2945, 'Revolut'],
             ['Liquidity', 'satispay', 201, 'Satispay'],
             ['Liquidity', 'paypal', 206, 'Paypal'],
             ['Liquidity', 'cash', 600, 'Cash'],
             ['Commodities', 'physical-gold', 100, 'Physical Gold'], // quantity in grams
         ],
-        prevMonthTotal: 424186
+        prevMonthTotal: 423221
     }
 }
 
